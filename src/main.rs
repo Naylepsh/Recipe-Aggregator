6e21ef11b3d8f67615cd372fcf04fda937fe3a36
Naylepsh <naylepsh@gmail.com>
2020-01-22 14:36:01 +0100

    add logger

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,13 @@
 // Load modules
 pub mod handlers;
 
-use actix_web::{web, App, HttpServer};
+use actix_web::{web, middleware, App, HttpServer};
 use tera::Tera;
-
-// async fn index(tmpl: web::Data<tera::Tera>, _req: HttpRequest) -> Result<HttpResponse, Error> {
-//     let s = tmpl.render("index.html", &tera::Context::new())
-//                 .map_err(|_| error::ErrorInternalServerError("Template error"))?;
-//     Ok(HttpResponse::Ok().content_type("text/html").body(s))
-// }
 
 #[actix_rt::main]
 async fn main() -> std::io::Result<()> {
     std::env::set_var("RUST_LOG", "actix_web=info");
+    env_logger::init();
 
     HttpServer::new(|| {
         let tera =
@@ -20,6 +15,7 @@ async fn main() -> std::io::Result<()> {
 
         App::new()
             .data(tera)
+            .wrap(middleware::Logger::default()) // enable logger
             .service(web::resource("/").route(web::get().to(handlers::landing::index)))
     })
     .bind("127.0.0.1:8080")?
