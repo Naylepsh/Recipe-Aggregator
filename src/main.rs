5f836ca51d07b8f04940717b2d1f6067e69453e0
Naylepsh <naylepsh@gmail.com>
2020-01-22 14:26:21 +0100

    refactor code structure (add handlers)

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,14 @@
-use actix_web::{error, web, App, Error, HttpResponse, HttpServer, HttpRequest};
+// Load modules
+pub mod handlers;
+
+use actix_web::{web, App, HttpServer};
 use tera::Tera;
 
-async fn index(tmpl: web::Data<tera::Tera>, _req: HttpRequest) -> Result<HttpResponse, Error> {
-    let s = tmpl.render("index.html", &tera::Context::new())
-                .map_err(|_| error::ErrorInternalServerError("Template error"))?;
-    Ok(HttpResponse::Ok().content_type("text/html").body(s))
-}
+// async fn index(tmpl: web::Data<tera::Tera>, _req: HttpRequest) -> Result<HttpResponse, Error> {
+//     let s = tmpl.render("index.html", &tera::Context::new())
+//                 .map_err(|_| error::ErrorInternalServerError("Template error"))?;
+//     Ok(HttpResponse::Ok().content_type("text/html").body(s))
+// }
 
 #[actix_rt::main]
 async fn main() -> std::io::Result<()> {
@@ -17,7 +20,7 @@ async fn main() -> std::io::Result<()> {
 
         App::new()
             .data(tera)
-            .service(web::resource("/").route(web::get().to(index)))
+            .service(web::resource("/").route(web::get().to(handlers::landing::index)))
     })
     .bind("127.0.0.1:8080")?
     .run()
